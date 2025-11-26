f3172cebcc00464df84362c55ca642c1d523e4a7
JC-677 <aramirez677@toromail.csudh.edu>
2025-11-26 12:02:44 -0800

    Create list.h
    Setting up for list.c

---
+++ list.h
@@ -0,0 +1,16 @@
+#ifndef LIST_H
+#define LIST_H
+#include "FrameWork.h"
+
+// The list where the students belong to
+extern Student student_list[MAX_STUDENTS];
+extern int student_count;
+
+//functions we'll be using
+void init_list(void);
+int get_student_list(void);
+Student* get_student_list(void);
+int add_to_list(Student student);
+int remove_from_list(int id);
+
+#endif
