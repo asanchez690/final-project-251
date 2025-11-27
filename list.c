8bf713be55e359404affb0d72e3d01c184843a97
JC-677 <aramirez677@toromail.csudh.edu>
2025-11-27 09:11:33 -0800

    Update list.c
    After major changes in OTHER files
    A few functions were removed as they were no longer being used

--- list.c
+++ list.c
@@ -9,16 +9,6 @@ void init_list(void)
     student_count = 0;  // reset the list
 }
 
-int get_student_count(void) 
-{
-    return student_count;
-}
-
-Student* get_student_list(void) 
-{
-    return student_list;
-}
-
 int add_to_list(Student student) 
 {
     if (student_count >= MAX_STUDENTS) 
@@ -29,18 +19,4 @@ int add_to_list(Student student)
     student_count++;
     return 0;  // Success
 }
-int remove_from_list(int id) 
-{
-    for (int i = 0; i < student_count; i++) 
-    {
-        if (student_list[i].idNum == id) {
-            // Shift all elements after the removed one
-            for (int j = i; j < student_count - 1; j++) {
-                student_list[j] = student_list[j + 1];
-            }
-            student_count--;
-            return 0;  // Success
-        }
-    }
-    return -1;  // not found :/
-}
+
