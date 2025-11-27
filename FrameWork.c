c6b7138c87b302c212455f9b8c97bacdca80775c
JC-677 <aramirez677@toromail.csudh.edu>
2025-11-27 08:55:26 -0800

    Update FrameWork.c
    Major changes to parameters
    Rework on listStudents function 
    
    Essentially it uses student_count & the list of students (which are global)

--- FrameWork.c
+++ FrameWork.c
@@ -1,22 +1,21 @@
 
 #include <stdio.h>
 #include "FrameWork.h"
+#include "list.h"
 #include <string.h>
 
 
-void addStudent(Student arr[], int *currentIndex )
+void addStudent(void)
 {
 
-  if(*currentIndex>MAX_STUDENTS) // this will lead to problems later down the line, especially if the user decides to delete
-  {//Have to find another way of finding empty spaces within array, Maybe another array that indicates available spots?
-  printf("Error: Maximum Number of Students have been added");
+  if(student_count > MAX_STUDENTS)  // this will lead to problems later down the line, especially if the user decides to delete
+  {                                 //Have to find another way of finding empty spaces within array, Maybe another array that indicates available spots?
+    printf("Error: Maximum Number of Students have been added");
   }
   else
   {
 
-
     Student s1; //student instance created
-
 
     printf("Enter ID Number: ");
     scanf("%d", &s1.idNum);
@@ -26,24 +25,34 @@ void addStudent(Student arr[], int *currentIndex )
 
     printf("Enter the Students Overall Grade: ");
     scanf("%f", &s1.averageScore);
-    //Maybe implement an if statement that validates that the index is valid
-    arr[*currentIndex] = s1;  // this adds the student to the correct place in 
-    (*currentIndex)++;
+    
+    //Now utilizes list functions to add students
+    if(add_to_list(s1) == 0) 
+    {
+        printf("Student added successfully!\n");
+    } else {
+        printf("Error adding student!\n");
+    }
+  
   }
 }
 
-void listStudents(Student arr[], int count)
+void listStudents(void)
 {
-    if(count == 0) 
-    {
-        printf("No students to display.\n");
-        return;
-    }
+  if(student_count == 0) 
+  {
+    printf("No students to display.\n");
+    return;
+  }
     
-    printf("\n=== STUDENT LIST ===\n");
-    for(int index = 0; index < count; index++)
-    {
-        printf("ID: %d, Name: %s, Grade: %.2f\n", arr[index].idNum, arr[index].name, arr[index].averageScore);
-    }
-    printf("===================\n\n");
+  printf("\n=== STUDENT LIST (%d students) ===\n", student_count);
+  for(int index = 0; index < student_count; index++)
+  {
+    printf("ID: %d, Name: %s, Grade: %.2f\n", 
+                       student_list[index].idNum, 
+                       student_list[index].name, 
+                       student_list[index].averageScore);
+  }
+  printf("===================\n\n");
+  
 }
