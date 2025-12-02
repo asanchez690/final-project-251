b34ef7bb384c652d3472db27dc2bf139368fdb8a
Steezyrosario <tonyserna200314@gmail.com>
2025-12-01 16:34:36 -0800

    Refactor student management functions in FrameWork.c

--- FrameWork.c
+++ FrameWork.c
@@ -1,58 +1,84 @@
 
+
 #include <stdio.h>
-#include "FrameWork.h"
-#include "list.h"
 #include <string.h>
+#include "Framework.h"
+#include "list.h"
 
-
+/*
+ * Adds a new student to the gradebook.
+ * Uses add_to_list from list.c to store the student.
+ */
 void addStudent(void)
 {
+    if (student_count >= MAX_STUDENTS) {
+        printf("Error: Maximum number of students reached\n");
+        return;
+    }
 
-  if(student_count > MAX_STUDENTS)  // this will lead to problems later down the line, especially if the user decides to delete
-  {                                 //Have to find another way of finding empty spaces within array, Maybe another array that indicates available spots?
-    printf("Error: Maximum Number of Students have been added");
-  }
-  else
-  {
-
-    Student s1; //student instance created
+    Student s1;
 
     printf("Enter ID Number: ");
     scanf("%d", &s1.idNum);
 
-    printf("Enter name of Student: "); 
-    scanf("%49s", s1.name); // setting the name of the student
+    printf("Enter name of Student: ");
+    scanf("%49s", s1.name);
 
-    printf("Enter the Students Overall Grade: ");
+    printf("Enter the Student's Overall Grade: ");
     scanf("%f", &s1.averageScore);
-    
-    //Now utilizes list functions to add students
-    if(add_to_list(s1) == 0) 
-    {
+
+    if (add_to_list(s1) == 0) {
         printf("Student added successfully!\n");
     } else {
         printf("Error adding student!\n");
     }
-  
-  }
 }
 
+/*
+ * Lists all students.
+ */
 void listStudents(void)
 {
-  if(student_count == 0) 
-  {
-    printf("No students to display.\n");
-    return;
-  }
-    
-  printf("\n=== STUDENT LIST (%d students) ===\n", student_count);
-  for(int index = 0; index < student_count; index++)
-  {
-    printf("ID: %d, Name: %s, Grade: %.2f\n", 
-                       student_list[index].idNum, 
-                       student_list[index].name, 
-                       student_list[index].averageScore);
-  }
-  printf("===================\n\n");
-  
+    if (student_count == 0) {
+        printf("No students to display.\n");
+        return;
+    }
+
+    printf("\n=== STUDENT LIST (%d students) ===\n", student_count);
+
+    for (int i = 0; i < student_count; i++) {
+        printf("ID: %d, Name: %s, Grade: %.2f\n",
+               student_list[i].idNum,
+               student_list[i].name,
+               student_list[i].averageScore);
+    }
+
+    printf("=================================\n\n");
+}
+
+/*
+ * Deletes a student by ID.
+ * This function was added to complete the delete feature.
+ *
+ * It calls remove_from_list from list.c.
+ * If the ID is found, it gets removed.
+ */
+void deleteStudent(void)
+{
+    if (student_count == 0) {
+        printf("No students to delete.\n");
+        return;
+    }
+
+    int id;
+    printf("Enter ID of student to delete: ");
+    scanf("%d", &id);
+
+    int result = remove_from_list(id);
+
+    if (result == 0) {
+        printf("Student deleted successfully.\n");
+    } else {
+        printf("Student with that ID was not found.\n");
+    }
 }
