dbf5d87d2443fc4001f905f3b718fde7cd2a0d7e
Steezyrosario <tonyserna200314@gmail.com>
2025-12-12 10:44:32 -0800

    Remove comments and clean up stats.h

--- stats.h
+++ stats.h
@@ -1,25 +1,18 @@
-//
-//  stats.h
-//  Final Project
-//
-//  Created by Anthony Rodriguez on 11/17/25.
-//
-
 #ifndef stats_h
 #define stats_h
 
 #include "Framework.h"
 
-/* Print statistics for the gradebook */
+// Print statistics for the gradebook
 void stats_print(const Student *students, int count);
 
-/* Compute the class average */
+// Compute the class average
 double stats_class_average(const Student *students, int count);
 
-/* Find index of student with highest average */
+// Find index of student with highest average
 int stats_index_of_max_avg(const Student *students, int count);
 
-/* Find index of student with lowest average */
+// Find index of student with lowest average
 int stats_index_of_min_avg(const Student *students, int count);
 
-#endif /* stats_h */
+#endif
